5468918db3886e5fc9018b6adc2af37215e25f09
vasilkrug <vkruglovec@gmail.com>
2023-08-17 23:09:52 +0300

    add total sale info

---
+++ js/basket.js
@@ -0,0 +1,33 @@
+const basketData = [
+    {
+        id: 1,
+        name: 'Футболка UZcotton мужская',
+        img: '',
+        size: '56',
+        color: 'белый',
+        company: 'ООО Вайлдберриз',
+        location: 'Коледино WB',
+        price: 1051,
+        sale: 49.67
+    },
+    {
+        id: 2,
+        name: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
+        img: '',
+        size: '56',
+        color: 'прозрачный',
+        company: 'OOO Мегапрофстиль',
+        location: 'Коледино WB',
+        price: 11500.235,
+        sale: 8.7
+    },
+    {
+        id: 3,
+        name: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные,Faber-Castell',
+        img: '',
+        company: 'ООО Вайлдберриз',
+        location: 'Коледино WB',
+        price: 475,
+        sale: 50
+    },
+]
