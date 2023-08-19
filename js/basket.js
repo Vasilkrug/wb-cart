da06937fdc16059a312bdfba79380c62f40f5ee5
vasilkrug <vkruglovec@gmail.com>
2023-08-19 12:25:45 +0300

    add basket items list

--- js/basket.js
+++ js/basket.js
@@ -1,33 +1,81 @@
-const basketData = [
-    {
-        id: 1,
-        name: 'Футболка UZcotton мужская',
-        img: '',
-        size: '56',
-        color: 'белый',
-        company: 'ООО Вайлдберриз',
-        location: 'Коледино WB',
-        price: 1051,
-        sale: 49.67
+import {basketData} from "./data.js";
+
+const model = {
+    state: basketData,
+}
+const view = {
+    render() {
+        const basketItemsList = document.querySelector('.basket-items-list');
+        let liItems = ''
+        model.state.forEach(item => {
+            liItems += `<li class="basket-item">
+                            <div class="basket-item-info-wrapper">
+                                <label class="checkbox-label" for=${'item-' + item.id}>
+                                    <input class="checkbox-input" id=${'item-' + item.id} type="checkbox" ${item.checked ? 'checked' : ''}>
+                                    <span class="checkbox"></span>
+                                    <img src=${item.img} alt="t-shirt">
+                                </label>
+                                <div class="basket-item-text">
+                                    <p>${item.name}</p>
+                                    <div class=${item.characters.length ? "basket-item-characters" : "basket-item-characters-hide"}>
+                                    ${item.characters.map(character => {
+                return `<span>${character}</span>`
+            }).join('')}
+                                    </div>
+                                    <div class="basket-item-location">
+                                        <div class="address">${item.location}</div>
+                                        <div class="company">
+                                            ${item.company}
+                                            <span class="tooltip"><img src="../assets/icons/info.png" alt="info"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="basket-item-controls-wrapper">
+                                <div class="basket-item-controls">
+                                    <div class="counter">
+                                        <button class="counter-button" data-direction="minus">−</button>
+                                        <div class="counter-value">${item.count}</div>
+                                        <button class="counter-button" data-direction="plus">+</button>
+                                    </div>
+                                    <div class="max-products">Осталось 2 шт.</div>
+                                    <div class="controls">
+                                        <img src="../assets/icons/like.svg" alt="like">
+                                        <img src="../assets/icons/delete.svg" alt="delete">
+                                    </div>
+                                </div>
+                                <div class="basket-item-price">
+                                    <div class="item-price">522 сом</div>
+                                    <div class="item-discount">1051 сом</div>
+                                </div>
+                            </div>
+                        </li>`
+        });
+        basketItemsList.innerHTML = liItems;
+
     },
-    {
-        id: 2,
-        name: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
-        img: '',
-        size: '56',
-        color: 'прозрачный',
-        company: 'OOO Мегапрофстиль',
-        location: 'Коледино WB',
-        price: 11500.235,
-        sale: 8.7
+    init() {
+        this.render()
     },
-    {
-        id: 3,
-        name: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные,Faber-Castell',
-        img: '',
-        company: 'ООО Вайлдберриз',
-        location: 'Коледино WB',
-        price: 475,
-        sale: 50
-    },
-]
+}
+const selectAllBtn = document.getElementById('select-all')
+selectAllBtn.addEventListener('change', () => {
+    let selected = false;
+    if (selectAllBtn.checked) {
+        selected = true
+    }
+    model.state = model.state.map(item => {
+        item.checked = selected
+        return item
+    })
+
+    view.render()
+
+})
+
+
+view.init()
+
+
+
+
