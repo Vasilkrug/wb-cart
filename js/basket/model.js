f513d9310680101e581ac098459f6933a4290f77
vasilkrug <vkruglovec@gmail.com>
2023-08-19 15:46:38 +0300

    change model

--- js/basket/model.js
+++ js/basket/model.js
@@ -1,5 +1,36 @@
 import {basketData} from "../data.js";
+import {view} from "./view.js";
 
 export const model = {
     state: basketData,
+    increment(index) {
+        const count = this.state[index].count;
+        const remains = this.state[index].remains;
+        if (count <= remains) {
+            this.state[index].count = this.state[index].count += 1
+            view.render()
+        }
+    },
+    decrement(index) {
+        const count = this.state[index].count;
+        if (count >= 2) {
+            this.state[index].count = this.state[index].count -= 1
+        }
+        view.render()
+    },
+    checkedAllItemsToggle(target){
+        let selected = false;
+        if (target.checked) {
+            selected = true
+        }
+        this.state = this.state.map(item => {
+            item.checked = selected
+            return item
+        })
+        view.render()
+    },
+    checkedItemToggle(index){
+        this.state[index].checked = !this.state[index].checked;
+
+    }
 }
