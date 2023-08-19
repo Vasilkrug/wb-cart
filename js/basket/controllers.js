cac72d37dddb3268cfb3310de4c63912a8ead6ea
vasilkrug <vkruglovec@gmail.com>
2023-08-19 15:46:28 +0300

    add counter controllers

--- js/basket/controllers.js
+++ js/basket/controllers.js
@@ -4,23 +4,29 @@ import {view} from "./view.js";
 view.init()
 const selectAllCheckbox = document.getElementById('select-all');
 const selectItemsCheckbox = document.querySelectorAll('[data-index]');
+const plusBtns = document.querySelectorAll('[data-direction="plus"]')
+const minusBtns = document.querySelectorAll('[data-direction="minus"]')
 
-    selectAllCheckbox.addEventListener('change', () => {
-    let selected = false;
-    if (selectAllCheckbox.checked) {
-        selected = true
-    }
-    model.state = model.state.map(item => {
-        item.checked = selected
-        return item
-    })
-    view.render()
+
+selectAllCheckbox.addEventListener('change', () => {
+    model.checkedAllItemsToggle(selectAllCheckbox)
 })
 
 selectItemsCheckbox.forEach(checkbox => {
-    checkbox.addEventListener('change',() => {
-        const index = checkbox.dataset.index;
-        model.state[index].checked = !model.state[index].checked;
+    checkbox.addEventListener('change', () => {
+        model.checkedItemToggle(checkbox.dataset.index)
     })
 })
 
+
+plusBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        model.increment(btn.dataset.counterId)
+    })
+})
+
+minusBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        model.decrement(btn.dataset.counterId)
+    })
+})
